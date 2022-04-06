56765aac6b82fc0bb8e9707785e43ee85f187cd7
Seb <84682690+KTUM@users.noreply.github.com>
2022-04-05 23:25:11 -0400

    design changes & bmap download Works!

--- commands/top.ts
+++ commands/top.ts
@@ -6,7 +6,8 @@ import axios from 'axios';
 import { format } from "morgan";
 import { InviteTargetType } from "discord.js/typings/enums";
 const base_url = 'https://osu.chronoskia.com';
-const avatar_url = 'https://a.chornoskia.com'
+const avatar_url = 'https://a.chornoskia.com';
+const gataribmap_url = 'https://osu.gatari.pw/d'
 export default {
     category : "Osutop",
     description : "Shows top play",
@@ -37,16 +38,33 @@ export default {
             var { data } = await axios.get(
                 `${base_url}/api/v1/users/full?id=` + id_check.id
             );
+            // nickname shit
+            var if_aka
+            if (!response.username_aka) {
+                if_aka = ''
+            } else {
+                if_aka = `AKA ${response.username_aka}`
+            }
             var { data } = await axios.get(
                 `${base_url}/api/v1/users/scores/best?id=${id_check.id}&mode=0&l=1`
             );
 
-            
+            var scores = data.scores
 
             try{
                 const embed = new MessageEmbed()
-                    .setThumbnail(`https://a.chronoskia.com/${id_check}`)
-                    .setDescription(`${response.username}\n Beatmap:${data.scores[0].beatmap.song_name}\nScore:${data.scores[0].score}\n Pp: ${data.scores[0].pp}  `)
+                .setAuthor({name: `${scores[0].beatmap.song_name} ⭐ ${Math.round(scores[0].beatmap.difficulty)}`, iconURL: `https://a.chronoskia.com/${id_check.id}`})
+                .setColor(`#aa3399`)
+                .setTitle(`Tap To Download`)
+                .setURL(`${gataribmap_url}/${scores[0].beatmap.beatmapset_id}`)
+                .setDescription(`${response.username} set score on ${scores[0].beatmap.song_name}`)
+                .addFields(
+                    {name: `Score`, value: `${scores[0].score}`},
+                    {name: `Pp`, value: `${scores[0].pp}` },
+                    {name: `Misses`, value: `${scores[0].count_miss}`},
+                    {name: `Combo`, value: `${scores[0].max_combo}/${scores[0].beatmap.max_combo}`}
+
+                )
                 return embed;
             } catch (error){
                 console.log(response.error)

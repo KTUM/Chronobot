573cfd62a9437c125a84adfe97007801a96be9f7
Seb <84682690+KTUM@users.noreply.github.com>
2022-04-06 21:07:33 -0400

    Update admin.ts

--- commands/admin.ts
+++ commands/admin.ts
@@ -1,2 +1,2 @@
 
-// Due to some issues with creating a ripple app token i have decided to nuke/quit working on admin commands for this bot (osu related)
+// Due to some issues with creating a ripple app token i have decided to nuke admin commands for this bot (osu related)
